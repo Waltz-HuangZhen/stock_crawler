c90ad6f2957cbdd1f71254ee6e64bf6db1b68714
Waltz <hearurheart@163.com>
2020-02-10 00:37:44 +0800

    Added Log model to admin site.

--- stock_crawler/urls.py
+++ stock_crawler/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
+from django.contrib import admin
+from django.urls import path, include
 
 
-router = DefaultRouter()
-urlpatterns = router.urls
-
-urlpatterns += [
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
