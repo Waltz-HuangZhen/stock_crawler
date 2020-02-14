c44838cc0af89d3e62fb68d257eed189194867eb
Waltz <hearurheart@163.com>
2020-02-15 00:24:08 +0800

    Added Celery tasks to crawl funds' worth.

--- stock_crawler/tasks.py
+++ stock_crawler/tasks.py
@@ -1,12 +1,19 @@
+import random
 import requests
 import json
+import time
+import re
+
+from lxml import etree
+
+from celery import group
 
 from stock_crawler.celery import app
 from stock_crawler.settings import (
-    NORMAL_TASK_TIMELIMIT, NORMAL_TASK_SOFTTIMELIMIT
+    NORMAL_TASK_TIMELIMIT, NORMAL_TASK_SOFTTIMELIMIT, CRAWLER_TASK_TIMELIMIT, CRAWLER_TASK_SOFTTIMELIMIT
 )
-from stock_crawler.models import Log, FundCode
-from stock_crawler.utils.utils import LogType
+from stock_crawler.models import Log, FundCode, FundCodeDayWorth
+from stock_crawler.utils.utils import LogType, requests_retry
 
 
 @app.task(queue='sc_crawler', soft_time_limit=NORMAL_TASK_SOFTTIMELIMIT, time_limit=NORMAL_TASK_TIMELIMIT)
@@ -39,3 +46,131 @@ def create_or_update_fund_code():
         return status, False
     status['message'] = status['message'].format(count, created_count, count-created_count)
     return status, True
+
+
+@app.task(queue='sc_crawler', soft_time_limit=CRAWLER_TASK_SOFTTIMELIMIT, time_limit=CRAWLER_TASK_TIMELIMIT)
+@Log.log_this(save_param=True)
+def crawl_all_funds(start_date=None, end_date=None):
+    status = {
+        'status': LogType.INFO,
+        'message': 'Successfully started to crawl {} stocks.'
+    }
+    count = 0
+    try:
+        codes = FundCode.objects.values_list('code', flat=True).all().iterator(chunk_size=500)
+        for code in codes:
+            crawl_fund.s(code, start_date, end_date).apply_async()
+            count += 1
+    except Exception as e:
+        status['message'] = e.__repr__()
+        status['status'] = LogType.ERROR
+        return status, False
+    status['message'] = status['message'].format(count)
+    return status, True
+
+
+@app.task(queue='sc_fund_crawler', soft_time_limit=CRAWLER_TASK_SOFTTIMELIMIT, time_limit=CRAWLER_TASK_TIMELIMIT)
+@Log.log_this(save_param=True)
+def crawl_fund(code, start_date=None, end_date=None):
+    status = {
+        'status': LogType.INFO,
+        'message': 'Successfully crawled {} pages(total {}, fail {}).'
+    }
+    base_url = 'http://fund.eastmoney.com/f10/F10DataApi.aspx' \
+               '?type=lsjz&code={code}&page={page}&per=50'
+    total_times = 10
+    task_group = []
+    page = 1
+    result = None
+    try:
+        if start_date:
+            base_url += '&sdate={}'.format(start_date)
+        if end_date:
+            base_url += '&edate={}'.format(end_date)
+
+        response_status, response = requests_retry(base_url.format(**locals()), total_times=total_times)
+        if response_status is not True:
+            status['message'] = 'Failed to crawl {}. Error: {}'.format(
+                code, response if response_status is None else response.text
+            )
+            status['status'] = LogType.ERROR
+            return status, False
+
+        data_dict = json.loads(re.sub('([a-zA-Z]*?):', '"\g<1>":', response.text[12:-1]))
+        task_group.append(crawl_fund_page.s(code, data_dict=data_dict))
+        for i in range(page + 1, data_dict['pages'] + 1):
+            page = i
+            task_group.append(crawl_fund_page.s(code, url=base_url.format(**locals())))
+
+        task_group = group(task_group)
+        result = task_group.apply_async()
+        while result.waiting():
+            time.sleep(2)
+    except Exception as e:
+        message = e.__repr__()
+        status['status'] = LogType.ERROR
+        return status, False
+    finally:
+        if result is not None:
+            if status['status'] == LogType.ERROR:
+                result.revoke()
+            successful_num = failed_num = 0
+            for r in result.results:
+                successful_num += int(r.result is True)
+                failed_num += int(r.result is False)
+            not_crawled_num = page - successful_num - failed_num
+            status['message'] = status['message'].format(successful_num, page, failed_num)
+        else:
+            status['message'] = 'Failed to crawl {}. Error: Tasks\' result is None.'.format(code)
+    return status, True
+
+
+@app.task(queue='sc_fund_page_crawler', soft_time_limit=NORMAL_TASK_SOFTTIMELIMIT, time_limit=NORMAL_TASK_TIMELIMIT)
+@Log.log_this(save_param=True)
+def crawl_fund_page(code, data_dict=None, url=None):
+    status = {
+        'status': LogType.INFO,
+        'message': ''
+    }
+    total_times = 10
+    try:
+        if url is not None:
+            time.sleep(random.randint(0, 15) / 10)
+            response_status, response = requests_retry(url, total_times=total_times)
+            if response_status is not True:
+                status['message'] = 'Failed to crawl {}. URL: {} \nError: {}'.format(
+                    code, url, response if response_status is None else response.text
+                )
+                status['status'] = LogType.ERROR
+                return status, False
+            data_dict = json.loads(re.sub('([a-zA-Z]*?):', '"\g<1>":', response.text[12:-1]))
+        if data_dict is not None:
+            html_data = etree.HTML(data_dict['content'])
+            for html_col in html_data.xpath('//table/tbody/tr'):
+                data = [str(d.xpath('string(.)')) for d in html_col]
+                if len(data) > 6:
+                    FundCodeDayWorth.objects.create(
+                        fund_code_id=code,
+                        date=data[0].replace('*', ''),
+                        nav=float(data[1] or 0),
+                        accnav=float(data[2] or 0),
+                        growth_rate=float(data[3][:-1] or 0),
+                        purchased=data[4],
+                        redemption=data[5],
+                        dividend=data[6]
+                    )
+                elif len(data) == 6:
+                    FundCodeDayWorth.objects.create(
+                        fund_code_id=code,
+                        date=data[0].replace('*', ''),
+                        ten_thousands_annual=float(data[1] or 0),
+                        seven_day_annual_yield=float(data[2][:-1] or 0),
+                        purchased=data[3],
+                        redemption=data[4],
+                        dividend=data[5]
+                    )
+    except Exception as e:
+        status['message'] = 'Failed to crawl {}. \nError: '.format(code) + e.__repr__()
+        status['status'] = LogType.ERROR
+        return status, False
+    return None, True
--- stock_crawler/utils/utils.py
+++ stock_crawler/utils/utils.py
@@ -1,3 +1,5 @@
+import requests
+
 from dataclasses import dataclass
 
 
@@ -6,3 +8,16 @@ class LogType:
     INFO: int=1
     WARNING: int=2
     ERROR: int=3
+
+
+def requests_retry(url, total_times=1, timeout=10):
+    times = 0
+    while (times := times + 1) <= total_times:
+        try:
+            response = requests.get(url, timeout=timeout)
+            if response.status_code == 200:
+                return True, response
+            elif response.status_code != 200 and times > total_times:
+                return False, response
+        except requests.RequestException as e:
+            return None, e.__repr__()
