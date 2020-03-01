7d2e14cf91d3bdedba2474fcb790bb860f231f36
Waltz <hearurheart@163.com>
2020-03-01 15:20:25 +0800

    Added mongodb supporting.

--- stock_crawler/utils/utils.py
+++ stock_crawler/utils/utils.py
@@ -2,6 +2,13 @@ import requests
 
 from dataclasses import dataclass
 
+from pymongo import MongoClient
+
+from stock_crawler.settings import MONGODB_URI, MONGODB_COLLECTION, MONGODB_DB
+
+
+MONGODB_CLIENT = MongoClient(MONGODB_URI)
+MONGODB_STOCK_CRAWLER = MONGODB_CLIENT[MONGODB_DB][MONGODB_COLLECTION]
 
 @dataclass
 class LogType:
